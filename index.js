f90da4fd48b5b783408806b39c71277ce77a37f4
ainnocent <andrew.innocent@cengage.com>
2018-06-21 19:34:26 -0400

    Fixed error where callback wouldn't invoke

--- index.js
+++ index.js
@@ -1,30 +1,26 @@
-// API key AIzaSyCVJFarvBKFpXZDJeAgbBbz4rOjFVvbfrQ
-
-var geocoder;
-var map;
 function initMap() {
-   var map = new google.maps.Map(document.getElementById('map'), {
-     zoom: 3,
-     center: {lat: 0, lng: 0}
-   });
-   var geocoder = new google.maps.Geocoder();
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 2,
+    center: {lat: 0, lng: 0}
+  });
+  var geocoder = new google.maps.Geocoder();
 
-   document.getElementById('submit').addEventListener('click', function() {
-     geocodeAddress(geocoder, map);
-   });
- }
+  document.getElementById('submit').addEventListener('click', function() {
+    geocodeAddress(geocoder, map);
+  });
+}
 
-function codeAddress() {
-  var address = document.getElementById('city-one').value; // prob won't need .value
-  geocoder.geocode( { 'address': address}, function(results, status) {
-    if (status == 'OK') {
-      map.setCenter(results[0].geometry.location);
+function geocodeAddress(geocoder, resultsMap) {
+  var address = document.getElementById('address').value;
+  geocoder.geocode({'address': address}, function(results, status) {
+    if (status === 'OK') {
+      resultsMap.setCenter(results[0].geometry.location);
       var marker = new google.maps.Marker({
-          map: map,
-          position: results[0].geometry.location
+        map: resultsMap,
+        position: results[0].geometry.location
       });
     } else {
-      alert('Geocode was not successful for the following reason: ' + status);
+      ('Geocode was not successful for the following reason: ' + status);
     }
   });
 }
