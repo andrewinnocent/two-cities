7a2ac9c2e9927b3016c42c5087f4881fb11ba4da
andrewinnocent <innocent.andrew@gmail.com>
2018-06-22 18:33:58 -0400

    Renders Relevant Values from API Response
    I realized the `results` param is returning json object with `status`,
    hence needing `results.status`. Successfully shows city names and/or error.

--- index.js
+++ index.js
@@ -4,58 +4,28 @@ $(document).ready()
 // Function to submit inputs to geocode api on form submit
 $('#form').submit(function () {
   event.preventDefault();
-  // assign city to variables
-  let city1 = $('#city-one').val()
-  let city2 = $('#city-two').val()
-  // console.log("City 1 is", city1);
-  // console.log("City 2 is", city2);
 
+  let city1 = $('#city-one').val();
+  let city2 = $('#city-two').val();
+
+  // City 1 GET request
   $.getJSON("https://maps.googleapis.com/maps/api/geocode/json?address=" + encodeURIComponent(city1) + "&key=AIzaSyCVJFarvBKFpXZDJeAgbBbz4rOjFVvbfrQ",
-    function(results, status) {
+    function(results) {
       console.log('Results is', results);
-      console.log('Status is', status); // Status is 'success', not 'OK'.
-          if (status === 'OK') { // Changing to 'success' returns undefined.
-            console.log("Connected to the API and the city is valid!"); // even though successful, return else result...
-            $('#city-one-location').text(results[0].geometry.location)
+          if (results.status === 'OK') { // results is returning json object with status.
+            $('#city-one-location').text("City 1: " + results.results[0].formatted_address)
           } else {
-            $('#city-one-location').text("Invalid input. Please enter a valid city.")
+            $('#city-one-location').text("Invalid City 1 input. Please enter a valid city.")
           }
         })
+  // City 2 GET request
   $.getJSON("https://maps.googleapis.com/maps/api/geocode/json?address=" + encodeURIComponent(city2) + "&key=AIzaSyCVJFarvBKFpXZDJeAgbBbz4rOjFVvbfrQ",
-    function(results, status) {
-        if (status === 'OK') {
-          $('#city-two-location').text(results[0].geometry.location)
+    function(results) {
+        if (results.status === 'OK') {
+          $('#city-two-location').text("City 2: " + results.results[0].formatted_address)
         } else {
-          $('#city-two-location').text("Invalid input. Please enter a valid city.")
+          $('#city-two-location').text("Invalid City 2 input. Please enter a valid city.")
         }
       })
-
     })
-
-// MAP
-// function initMap() {
-//   var map = new google.maps.Map(document.getElementById('map'), {
-//     zoom: 2,
-//     center: {lat: 0, lng: 0}
-//   });
-//   var geocoder = new google.maps.Geocoder();
-//
-//   document.getElementById('submit').addEventListener('click', function() {
-//     geocodeAddress(geocoder, map);
-//   });
-// }
-//
-// function geocodeAddress(geocoder, resultsMap) {
-//   var address = document.getElementById('address').value;
-//   geocoder.geocode({'address': address}, function(results, status) {
-//     if (status === 'OK') {
-//       resultsMap.setCenter(results[0].geometry.location);
-//       var marker = new google.maps.Marker({
-//         map: resultsMap,
-//         position: results[0].geometry.location
-//       });
-//     } else {
-//       ('Geocode was not successful for the following reason: ' + status);
-//     }
-//   });
-// }
+    
